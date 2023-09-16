e76014cceb26f76427033949bb79020ee654e96e
Matias <matute_11@hotmail.com.ar>
2023-09-16 00:08:27 -0300

    Agregando funciones para leer nombres

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
-const nombre = document.getElementById("text-name").value;
+const nombre = document.getElementById("text-name");
+const botonNombre = document.getElementById("btn-name");
+
+//Funcion para mandar el nombre con el boton (click)
+botonNombre.addEventListener('click', () => {
+    const textoIngresado = nombre.value;
+    if(textoIngresado){
+        console.log(textoIngresado)
+    }else{
+        alert("No pusiste nada papu");
+    }
+    nombre.value = '';
+});
+
+//
+nombre.addEventListener('keydown', function(event) {
+    // Verifica si la tecla presionada es "Enter" (código de tecla 13)
+    if (event.keyCode === 13) {
+      // Obtiene el valor del input
+      const nombre2 = nombre.value;
+      // Muestra el nombre en la consola
+      console.log(nombre2);
+      // Limpia el input
+      nombre.value = '';
+    }
+  });
 
 const textos = [
     "Hello [CUSTOMER]! Please confirm if the issue was fixed after the changes that have been made. Kind regards",
